cce9f886293aa29ae036019f99a8b12199df7056
dix <william.j.dix@gmail.com>
2015-11-13 18:39:19 -0500

    Fixes incorrect license file.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject korma-encrypted "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.postgresql/postgresql "9.2-1002-jdbc4"]
                  [korma "0.4.0"]])
